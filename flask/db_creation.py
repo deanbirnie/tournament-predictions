6d91ea29aeafa4d6e133163508f76a99db99b64e
deanbirnie <deanicdl@gmail.com>
2023-09-11 11:33:00 +0000

    Creeated DB

--- flask/db_creation.py
+++ flask/db_creation.py
@@ -1,49 +1,28 @@
+#!/usr/bin/env python3
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-# Initialize Flask application
 app = Flask(__name__)
-
-# Configure SQLite database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///rugby_predictions.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# Initialize SQLAlchemy
 db = SQLAlchemy(app)
 
-# Define Matches table
 class Match(db.Model):
-    match_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    date = db.Column(db.String, nullable=False)
-    team_a = db.Column(db.String, nullable=False)
-    team_b = db.Column(db.String, nullable=False)
+    match_id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.String(50))
+    team_a = db.Column(db.String(50))
+    team_b = db.Column(db.String(50))
 
-    def as_dict(self):
-        return {
-            'match_id': self.match_id,
-            'date': self.date,
-            'team_a': self.team_a,
-            'team_b': self.team_b,
-        }
-
-# Define Predictions table
 class Prediction(db.Model):
-    prediction_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    prediction_id = db.Column(db.Integer, primary_key=True)
     match_id = db.Column(db.Integer, db.ForeignKey('match.match_id'), nullable=False)
-    name = db.Column(db.String, nullable=False)
-    timestamp = db.Column(db.String, nullable=False)
-    team_a_score = db.Column(db.Integer, nullable=False)
-    team_b_score = db.Column(db.Integer, nullable=False)
+    name = db.Column(db.String(100))
+    team_a_score = db.Column(db.Integer)
+    team_b_score = db.Column(db.Integer)
+    timestamp = db.Column(db.DateTime)
 
-    def as_dict(self):
-        return {
-            'prediction_id': self.prediction_id,
-            'match_id': self.match_id,
-            'name': self.name,
-            'timestamp': self.timestamp,
-            'team_a_score': self.team_a_score,
-            'team_b_score': self.team_b_score,
-        }
-
-# Create the database and tables
-db.create_all()
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
